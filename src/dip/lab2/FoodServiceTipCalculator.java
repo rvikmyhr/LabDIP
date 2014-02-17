8bcaf8a4171def693ad9673f3ef67322bc9ddbaf
Ron <rvikmyhr@my.wctc.edu>
2014-02-17 14:27:10 -0600

    Finalized

--- src/dip/lab2/FoodServiceTipCalculator.java
+++ src/dip/lab2/FoodServiceTipCalculator.java
@@ -9,7 +9,8 @@ package dip.lab2;
  * @author your name goes here
  */
 public class FoodServiceTipCalculator implements TipCalculator {
-    private static final double MIN_BILL = 0.00;
+    private static double MIN_BILL = 0.00;
+    private static double MAX_BILL = 100.00;
     private static final String BILL_ENTRY_ERR =
             "Error: bill must be greater than or equal to " + MIN_BILL;
 //    private static final double GOOD_RATE = 0.20;
